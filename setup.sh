c800aba275bbb00ab7d4dbddb9bf4257be8608fe
Tianxi Ku <txku@thoughtworks.com>
2018-07-24 00:11:20 +0800

    fix bug that go agent cannot read register key when go server is not fully started

--- setup.sh
+++ setup.sh
@@ -4,6 +4,14 @@ function extractAgentAutoRegistryKey {
     echo $(grep 'agentAutoRegisterKey' GoServerData/config/cruise-config.xml | awk -F"agentAutoRegisterKey=\"" '{print $2}' | awk -F"\" webhookSecret" '{print $1}')
 }
 
+function waitUntilGoServerStarts {
+    while ! grep -q agentAutoRegisterKey GoServerData/config/cruise-config.xml; do
+        echo 'Go server startup in progress'
+        sleep 5
+    done
+    extractAgentAutoRegistryKey
+}
+
 function startGoAgent {
     docker run -d \
     --net=host \
@@ -33,6 +41,7 @@ docker build -t goagent-with-docker:latest .
 
 #start docker server and agent
 startGoServer \
+&& waitUntilGoServerStarts \
 && echo "go server started" \
 && startGoAgent \
 && echo "go agent started"
