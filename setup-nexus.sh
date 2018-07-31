2548dba604349138097b1871860ce49b8173abf8
Tianxi Ku <txku@thoughtworks.com>
2018-07-31 23:10:30 +0800

    update port

--- setup-nexus.sh
+++ setup-nexus.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d -u root -v $(pwd)/NexusData:/nexus-data -p 5000:5000 -p 8090:8081 --name nexus sonatype/nexus3
+docker run -d -v $(pwd)/NexusData:/nexus-data -p 5000:5000 -p 9091:8081 --name nexus sonatype/nexus3
